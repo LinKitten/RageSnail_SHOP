40631293a1d2effdcce2ed9a0894b286b3c182a1
LinKitten <2634024527@qq.com>
2022-12-06 22:27:47 +0800

    新建组件

---
+++ ragesnailshop_app/src/pages/Register/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>注册  没有底部的footer组件</div>
+</template>
+
+<script>
+export default {
+    nanme:"Register"
+}
+</script>
+
+<style>
+
+</style>
---
+++ ragesnailshop_app/src/pages/Search/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Search搜索</div>
+</template>
+
+<script>
+export default {
+    name:"Search"
+}
+</script>
+
+<style>
+
+</style>
