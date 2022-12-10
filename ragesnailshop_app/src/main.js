7d0f4f24760574466d552239f010e2454c9baac9
LinKitten <2634024527@qq.com>
2022-12-10 21:59:50 +0800

    路由

--- ragesnailshop_app/src/main.js
+++ ragesnailshop_app/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-Vue.config.productionTip = false
+//引入路由配置文件
+import router from "@/router"
+// Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  //注册路由
+  //当这里书写router的时候，组件身上都拥有了$route,$router属性 
+  router,
 }).$mount('#app')
