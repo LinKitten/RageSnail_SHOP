028276593d06315af92e8cf619846e473a1d85c6
LinKitten <2634024527@qq.com>
2022-12-20 16:52:49 +0800

    TypeNav三级联动全局组件完成

--- ragesnailshop_app/src/main.js
+++ ragesnailshop_app/src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+// 组成全局组件
+// 三级联动组件 -----全局组件
+import TypeNav from '@/pages/Home/TypeNav'
+// 第一个参数：全局组件的名字 第二个参数：哪一个组件
+Vue.component(TypeNav.name,TypeNav)
+
 //引入路由配置文件
 import router from "@/router"
 // Vue.config.productionTip = false
--- ragesnailshop_app/src/pages/Home/index.vue
+++ ragesnailshop_app/src/pages/Home/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>首页</div>
+  <div>
+
+    <!--三级联动全局组件 （在main.js注册为全局组件，因此不用引入） -->
+    <TypeNav></TypeNav>
+  </div>
 </template>
 
 <script>
