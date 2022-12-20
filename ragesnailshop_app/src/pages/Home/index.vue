15e49ef80f0f68929aa2045be913417d76b6d212
LinKitten <2634024527@qq.com>
2022-12-20 22:11:02 +0800

    新建列表组件

--- ragesnailshop_app/src/pages/Home/index.vue
+++ ragesnailshop_app/src/pages/Home/index.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-
     <!--三级联动全局组件 （在main.js注册为全局组件，因此不用引入） -->
     <TypeNav></TypeNav>
+    <ListContainer/>
   </div>
 </template>
 
 <script>
+// 引入其余的组件
+import ListContainer from "@/pages/Home/ListContainer"
 export default {    
-    name:"Home"
+    name:"Home",
+    components:{
+      ListContainer
+    }
 }
 </script>
 
