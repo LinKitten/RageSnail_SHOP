2e72d1239baf821872a89d39eefd70fe5e4b29d5
LinKitten <2634024527@qq.com>
2022-12-20 22:33:15 +0800

    新增今日推荐组件

--- ragesnailshop_app/src/pages/Home/index.vue
+++ ragesnailshop_app/src/pages/Home/index.vue
@@ -1,19 +1,27 @@
 <template>
   <div>
     <!--三级联动全局组件 （在main.js注册为全局组件，因此不用引入） -->
-    <TypeNav></TypeNav>
-    <ListContainer/>
+    <!-- 商品分类导航 -->
+    <TypeNav/>
+
+    <!-- 列表 -->
+    <ListContainer />
+    <!-- 今日推荐 -->
+    <Recommend />
+
   </div>
 </template>
 
 <script>
 // 引入其余的组件
 import ListContainer from "@/pages/Home/ListContainer"
-export default {    
-    name:"Home",
-    components:{
-      ListContainer
-    }
+import Recommend from "@/pages/Home/Recommend"
+export default {
+  name: "Home",
+  components: {
+    ListContainer,
+    Recommend,
+  }
 }
 </script>
 
