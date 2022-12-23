5775b68f520876304e48c795f1059a0211673da7
LinKitten <2634024527@qq.com>
2022-12-23 13:34:43 +0800

    添加组件

--- ragesnailshop_app/src/pages/Home/index.vue
+++ ragesnailshop_app/src/pages/Home/index.vue
@@ -10,6 +10,13 @@
     <Recommend />
     <!-- 商品排行 -->
     <Rank/>
+    <!--猜你喜欢 -->
+    <Like/>
+    <!-- 楼层 -->
+    <Floor/>
+    <Floor/>
+    <!-- 商标 -->
+    <Brand/>
 
   </div>
 </template>
@@ -19,12 +26,19 @@
 import ListContainer from "@/pages/Home/ListContainer"
 import Recommend from "@/pages/Home/Recommend"
 import Rank from "@/pages/Home/Rank"
+import Like from "@/pages/Home/Like"
+import Floor from "@/pages/Home/Floor"
+import Brand from "@/pages/Home/Brand"
+
 export default {
   name: "Home",
   components: {
     ListContainer,
     Recommend,
     Rank,
+    Like,
+    Floor,
+    Brand,
   }
 }
 </script>
