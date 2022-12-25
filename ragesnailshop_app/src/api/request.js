d02a0c44683827b6191c494c972c1c09e2702654
LinKitten <2634024527@qq.com>
2022-12-25 23:21:32 +0800

    使用nprogress进度条

--- ragesnailshop_app/src/api/request.js
+++ ragesnailshop_app/src/api/request.js
@@ -1,6 +1,10 @@
 // 对于axios进行二次封装
 
 import axios from "axios";
+// 引入进度条
+import nProgress from "nprogress";
+// 引入进度条的演示
+import "nprogress/nprogress.css";
 
 // 1、利用axios对象的方法create，去创建一个axios实例
 const requests = axios.create({
@@ -12,11 +16,15 @@ const requests = axios.create({
 });
 // 请求拦截器；在发请求之前，请求拦截器可以检测到,可以在请求发出去之前做一些事情
 requests.interceptors.request.use((config)=>{
+    // 添加进度条(进度条开始)
+    nProgress.start();
     // config:配置对象，对象里面有一个属性很重要，header请求头
     return config
 });
 // 响应拦截器
 requests.interceptors.response.use((res)=>{
+    // 添加进度条(进度条结束)
+    nProgress.done();
     // 成功的回调函数：服务器响应数据回来后，响应拦截器可以检测到，可以做一些事情
     return res.data;
 },(error)=>{
