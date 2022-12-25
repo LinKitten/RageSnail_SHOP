e802b9f5e66a4b3e503db8210bd33ffa6ed71314
LinKitten <2634024527@qq.com>
2022-12-25 23:06:01 +0800

    配置代理跨域

--- ragesnailshop_app/vue.config.js
+++ ragesnailshop_app/vue.config.js
@@ -2,4 +2,12 @@
 module.exports = {
   // transpileDependencies: true,
   lintOnSave: false,//关闭eslint校验工具
+  // 代理跨域
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://gmall-h5-api.atguigu.cn',
+      },
+    },
+  },
 }
