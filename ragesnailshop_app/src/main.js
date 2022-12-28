3a312c903b4838fa22e3f994e0a457a2d2ef2bff
LinKitten <2634024527@qq.com>
2022-12-28 23:18:37 +0800

    三级联动组件

--- ragesnailshop_app/src/main.js
+++ ragesnailshop_app/src/main.js
@@ -3,14 +3,13 @@ import App from './App.vue'
 
 // 组成全局组件
 // 三级联动组件 -----全局组件
-import TypeNav from '@/pages/Home/TypeNav'
+import TypeNav from '@/components/TypeNav'
 // 第一个参数：全局组件的名字 第二个参数：哪一个组件
 Vue.component(TypeNav.name,TypeNav)
 
 //引入路由配置文件
 import router from "@/router"
 // Vue.config.productionTip = false
-
 // 测试api
 import {reCategoryList} from "@/api";
 reCategoryList();
