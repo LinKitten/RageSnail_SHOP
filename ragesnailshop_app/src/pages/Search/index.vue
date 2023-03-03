754286e118f4c84f24e5cbc74f62abd499ea666a
LinKitten <2634024527@qq.com>
2023-03-03 15:58:01 +0800

    getters使用- store中的计算属性

--- ragesnailshop_app/src/pages/Search/index.vue
+++ ragesnailshop_app/src/pages/Search/index.vue
@@ -6,8 +6,15 @@
 </template>
 
 <script>
+ //引入mapGetters
+ import {mapGetters} from 'vuex'
 export default {
     name:"Search",
+    computed:{
+      //使用mapGetters，参数是一个数组，数组的元素对应getters中的函数名
+      ...mapGetters(['goodsList'])
+    },
+  
 }
 </script>
 
--- ragesnailshop_app/src/store/search/index.js
+++ ragesnailshop_app/src/store/search/index.js
@@ -1,12 +1,33 @@
+import {reqGetSearchInfo} from '@/api';
 // search模块的小仓库
 // state：仓库存储数据的地方
-const state = {};
+const state = {
+    searchList:{},
+};
 // mutations:修改state的唯一手段
-const mutations = {};
+const mutations = {
+    SEARCHLIST(state,searchList){
+        state.searchList = searchList
+    }
+};
 // actions：处理actions，可以书写自己的业务逻辑也可以处理异步
-const actions = {};
+const actions = {
+     //第二个参数data默认是一个空对象
+     async getSearchListr({commit},data={}){
+        let result = await reqGetSearchInfo(data)
+
+        if(result.code === 200){
+            commit("SEARCHLIST",result.data)
+        }
+    }
+};
 // getters:理解为计算属性，用于简化仓库数据，让组件获取仓库的数据更方便
-const getters = {};
+const getters = {
+    goodsList(state){
+        //网络出现故障时应该将返回值设置为空
+        return state.searchList.goodsList||[]
+    },
+};
 
 export default {
     state,
