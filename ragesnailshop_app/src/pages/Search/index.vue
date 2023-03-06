059f0c932e948c0676580a91fcb924b02564d5f9
LinKitten <2634024527@qq.com>
2023-03-06 22:39:51 +0800

    利用路由信息变化实现动态搜索

--- ragesnailshop_app/src/pages/Search/index.vue
+++ ragesnailshop_app/src/pages/Search/index.vue
@@ -14,7 +14,19 @@ export default {
       //使用mapGetters，参数是一个数组，数组的元素对应getters中的函数名
       ...mapGetters(['goodsList'])
     },
-  
+    watch:{
+      $route(newValue,oldValue){
+        Object.assign(this.searchParams,this.$route.query,this.$route.params)
+        this.searchInfo()
+        //如果下一次搜索时只有params参数，拷贝后会发现searchParams会保留上一次的query参数
+        //所以每次请求结束后将相应参数制空
+        this.searchParams.category1Id = '';
+        this.searchParams.category2Id = '';
+        this.searchParams.category3Id = '';
+        this.$route.params.keyword = '';
+      }
+    },
+
 }
 </script>
 
